b880469aef6c58d97d4261614580cf5fb3d93b12
manhpdhe151290 <manhpdhe151290@fpt.edu.vn>
2022-11-03 14:01:34 +0700

    update
    1

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -8,18 +8,20 @@ public class LevelLoader : MonoBehaviour
 {
     public GameObject loadingScreen;
     public Slider slider;
-   public void LoadLevel(int sceneIndex)
+    public Text Textprogress;
+    public void LoadLevel(int sceneIndex)
     {
         StartCoroutine(LoadAsynchronously(sceneIndex));
         loadingScreen.SetActive(true);
-    }   
-    IEnumerator LoadAsynchronously (int sceneIndex)
+    }
+    IEnumerator LoadAsynchronously(int sceneIndex)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
         while (!operation.isDone)
         {
             float progress = Mathf.Clamp01(operation.progress / .9f);
             slider.value = progress;
+            Textprogress.text = progress * 100f + "%";
             yield return null;
         }
     }
